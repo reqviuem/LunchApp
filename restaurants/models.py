447d061ac405a8007d1016cce6d50b9105b4c792
reqvi <slavil.larin@gmail.com>
2026-04-30 18:24:59 +0200

    Added apps to project settings

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+
+class Menu(models.Model):
+    restaurant_id = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE
+    )
+    date = models.DateField()
+    title = models.CharField(max_length=100)
+
